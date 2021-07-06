958422ddc8392009b53644000326e5fe984b6ed9
DESKTOP-1EEHAE2\LGUser <youngine012@naver.com>
2021-07-06 15:05:52 +0900

    Add distance calculation

--- lib/mapPage/jupggingEnd.dart
+++ lib/mapPage/jupggingEnd.dart
@@ -15,13 +15,14 @@ import 'package:provider/provider.dart';
 class JupggingEnd extends StatefulWidget {
   final RunningInfo run ;
   final List<LatLng> route;
-  final LatLng spoint;
+  final LatLng departure;
+  final double distance;
 
   @override
   State<StatefulWidget> createState() => _JupggingEnd();
   //run = ModalRoute.of(context)!.settings.arguments as RunningInfo;
   //run = ModalRoute.of(context).settings.arguments;
-  JupggingEnd({Key key, @required this.run, this.route, this.spoint}) : super(key: key);
+  JupggingEnd({Key key, @required this.run, this.route, this.departure,this.distance}) : super(key: key);
 }
 
 class _JupggingEnd extends State<JupggingEnd> {
@@ -44,12 +45,14 @@ class _JupggingEnd extends State<JupggingEnd> {
   @override
   Widget build(BuildContext context) {
     final info = ModalRoute.of(context).settings.arguments as JupggingEnd;
-    points = info.route; //polyline point
-    start_point = info.spoint; //시작점 받기
+    points = info.route; //경로 polyline point
+    start_point = info.departure; //시작점
+    distance= info.distance; //거리
     var m = info.run.minutes;
     var s = info.run.seconds;
 
-    _markers.add(Marker( //시작위치 마커
+    //시작위치 마커
+    _markers.add(Marker(
         markerId: MarkerId("MyStartPosition"),
         position: LatLng(start_point.latitude, start_point.longitude),
         infoWindow: InfoWindow(title:'Start Position',snippet:'Start Running!!')
@@ -64,15 +67,16 @@ class _JupggingEnd extends State<JupggingEnd> {
                   color: Colors.white,
                   height: (MediaQuery.of(context).size.height-50)*0.85,
                   child: Center(
-                    //child: googleMapUI(),
-                    child:_image == null ? Text('No Image') : Image.file(File(_image.path)),
+                    child: googleMapUI(),
+                    //child:_image == null ? Text('No Image') : Image.file(File(_image.path)),
                   )
               ),
               Container(  //달린 거리, 시간 나오는 부분
                 height: (MediaQuery.of(context).size.height-50)*0.15+50,
                 color: Colors.white,
-                child: Text('$m 분 $s 초',textAlign: TextAlign.center,style:TextStyle(fontSize: 20)),  //시간 계산
-              ),]
+                child: Text('$distance km $m 분 $s 초',textAlign: TextAlign.center,style:TextStyle(fontSize: 20)),  //시간 계산
+                ),
+            ]
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -127,14 +131,6 @@ class _JupggingEnd extends State<JupggingEnd> {
       if(model.locationPosition != null){
 
         end_point = model.locationPosition; //마지막 목적지 위치
-        if(distance == 0.0) {
-          distance = double_coordinateDistance(
-              start_point.latitude, start_point.longitude, end_point.latitude,
-              end_point.longitude); //거리계산
-          distance = double.parse(distance.toStringAsFixed(2)); //소수점 한자리
-          print('거리 $distance km');
-        }
-
         _markers.add(Marker( //도착위치 마커
             markerId: MarkerId("MyEndPosition"),
             position: LatLng(end_point.latitude, end_point.longitude),
@@ -200,14 +196,6 @@ class _JupggingEnd extends State<JupggingEnd> {
     });
   }
 
- double_coordinateDistance(lat1,lon1,lat2,lon2){
-   var p = 0.017453292519943295;
-   var c = cos;
-   var a = 0.5 -
-       c((lat2 - lat1) * p) / 2 +
-       c(lat1 * p) * c(lat2 * p) * (1 - c((lon2 - lon1) * p)) / 2;
-   return 12742 * asin(sqrt(a));
- }
 
 }
 

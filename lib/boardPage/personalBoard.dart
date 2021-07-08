8980467f4de04c4afc12556d1f9d7ab05a79e4f7
hye_ppy <1569369@naver.com>
2021-07-08 16:13:01 +0900

    Update personalBoard.dart

--- lib/boardPage/personalBoard.dart
+++ lib/boardPage/personalBoard.dart
@@ -37,7 +37,6 @@ class _PersonalBoard extends State<PersonalBoard> {
         _imgUrl.add(ImageURL.fromSnapshot(event.snapshot));
       });
     });
-
   }
 
   // Future<List> ImageDownload() async{
@@ -52,113 +51,139 @@ class _PersonalBoard extends State<PersonalBoard> {
   // }
   GridView _imageGrid() {
     return GridView.count(
-
       shrinkWrap: true,
       crossAxisCount: 3,
       childAspectRatio: 1,
       children: [
-        Image.network(_imgUrl[0].url,fit: BoxFit.fill),
-        Image.network(_imgUrl[1].url,fit: BoxFit.fill)
+        Image.network(_imgUrl[0].url, fit: BoxFit.fill),
+        Image.network(_imgUrl[1].url, fit: BoxFit.fill)
       ],
     );
   }
 
   Widget build(BuildContext context) {
-    print(_imgUrl[0].createTime);
+    //_imgUrl=List.from(_imgUrl.reversed);
+    print('ddddddd' + _imgUrl[0].url);
     return Scaffold(
       body: Container(
-        child: Column(
-            children: [
-              Container(
-                color: Colors.deepOrange,
-                width: MediaQuery
-                    .of(context)
-                    .size
-                    .width,
-                height: 200,
-                child: Column(
+        child: Column(children: [
+          Container(
+            color: Colors.deepOrange,
+            width: MediaQuery.of(context).size.width,
+            height: 200,
+            child: Column(
+              children: [
+                Row(
                   children: [
-                    Row(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.fromLTRB(35, 35, 35, 20),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(55.0),
-                            child: Image.asset(
-                              'image/tree.jpg',
-                              width: 90,
-                              height: 90,
-                              fit: BoxFit.fill,
-                            ),
-                          ),
-                        ),
-                        Container(
-                            child: Column(
-                              children: [
-                                Padding(
-                                    padding: EdgeInsets.fromLTRB(
-                                        30, 50, 35, 20),
-                                    child: Text("Running",style:TextStyle(fontWeight: FontWeight.bold,color: Colors.white))
-                                ),
-                                Padding(
-                                    padding: EdgeInsets.fromLTRB(35, 0, 35, 20),
-                                    child: Text("10km",style:TextStyle(color: Colors.white))
-                                ),
-                              ],
-                            )
-                        ),
-                        Container(
-                            child: Column(
-                              children: [
-                                Padding(
-                                    padding: EdgeInsets.fromLTRB(
-                                        20, 50, 35, 20),
-                                    child: Text("Time",style:TextStyle(fontWeight: FontWeight.bold,color: Colors.white))
-                                ),
-                                Padding(
-                                    padding: EdgeInsets.fromLTRB(20, 0, 35, 20),
-                                    child: Text("14h",style:TextStyle(color: Colors.white))
-                                ),
-                              ],
-                            )
-                        ),
-                      ],
-                    ),
                     Padding(
-                      padding: EdgeInsets.fromLTRB(100, 0, 100, 0),
-                      child: RaisedButton(
-                        color : Colors.white,
-                        child: Row(
-                          children: <Widget>[
-                            Icon(Icons.settings, color: Colors.deepOrange,),
-                            Text('  setting',style:TextStyle(color: Colors.deepOrange))
-                          ],
-                          mainAxisAlignment: MainAxisAlignment.center,
+                      padding: EdgeInsets.fromLTRB(35, 35, 35, 20),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(55.0),
+                        child: Image.asset(
+                          'image/tree.jpg',
+                          width: 90,
+                          height: 90,
+                          fit: BoxFit.fill,
                         ),
-                        onPressed: () {
-                          reference.child(id).onChildAdded.listen((event) {
-                            user = User.fromSnapshot(event.snapshot);
-                            Navigator.of(context)
-                                .pushNamed('/mypage', arguments: user);
-                          });
-                        },
                       ),
                     ),
+                    Container(
+                        child: Column(
+                      children: [
+                        Padding(
+                            padding: EdgeInsets.fromLTRB(30, 50, 35, 20),
+                            child: Text("Running",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white))),
+                        Padding(
+                            padding: EdgeInsets.fromLTRB(35, 0, 35, 20),
+                            child: Text("10km",
+                                style: TextStyle(color: Colors.white))),
+                      ],
+                    )),
+                    Container(
+                        child: Column(
+                      children: [
+                        Padding(
+                            padding: EdgeInsets.fromLTRB(20, 50, 35, 20),
+                            child: Text("Time",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white))),
+                        Padding(
+                            padding: EdgeInsets.fromLTRB(20, 0, 35, 20),
+                            child: Text("14h",
+                                style: TextStyle(color: Colors.white))),
+                      ],
+                    )),
                   ],
                 ),
-              ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(100, 0, 100, 0),
+                  child: RaisedButton(
+                    color: Colors.white,
+                    child: Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.settings,
+                          color: Colors.deepOrange,
+                        ),
+                        Text('  setting',
+                            style: TextStyle(color: Colors.deepOrange))
+                      ],
+                      mainAxisAlignment: MainAxisAlignment.center,
+                    ),
+                    onPressed: () {
+                      reference.child(id).onChildAdded.listen((event) {
+                        user = User.fromSnapshot(event.snapshot);
+                        Navigator.of(context)
+                            .pushNamed('/mypage', arguments: user);
+                      });
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+          AnimatedContainer(
+              transform: Matrix4.translationValues(0, 0, 0),
+              duration: Duration(milliseconds: 10),
+              curve: Curves.linear,
+              child: _imgUrl.length == 0
+                  ? CircularProgressIndicator()
+                  : GridView.builder(
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 3),
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          //child: GridTile(
+                            child: Container(
+                              width: 200,
+                              //padding: EdgeInsets.only(top: 20, bottom: 20),
+                              child: SizedBox(
+                                child: GestureDetector(
+                                  onTap: () {
+                                    //누르면 인스타 개인 게시물처럼 보기
+                                  },
+                                  child:
+                                  // Text('ddd'),
+                                  Image.network(_imgUrl[index].url,
+                                      fit: BoxFit.fill),
+                                ),
+                              ),
+                            ),
+                          //),
+                        );
+                      },
+                itemCount: _imgUrl.length,
+                    )
 
-              AnimatedContainer(
-                transform: Matrix4.translationValues(0, 0, 0),
-                duration: Duration(milliseconds: 10),
-                curve: Curves.linear,
-                child:
-                Text('_imgUrl[0].url'),
-
-                //_imageGrid(),
+              //_imageGrid(),
               ),
-            ]
-        ),
+        ]),
       ),
     );
   }

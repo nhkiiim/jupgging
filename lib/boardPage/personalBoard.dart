0c996c32ee4ed8c72ace1febb615605bc24bc611
DESKTOP-1EEHAE2\LGUser <youngine012@naver.com>
2021-07-10 18:33:15 +0900

    update url upload function

--- lib/boardPage/personalBoard.dart
+++ lib/boardPage/personalBoard.dart
@@ -22,12 +22,15 @@ class _PersonalBoard extends State<PersonalBoard> {
   @override
   void initState() {
     super.initState();
-
     id = 'happy123';
+
     _database = FirebaseDatabase(databaseURL: _databaseURL);
     reference = _database.reference().child('user');
     referenceImg = _database.reference().child('image');
 
+    // ImageDownload().then((value)=>{
+    //   setState(() {_imgUrl = value;})
+    // });
     referenceImg.child(id).onChildAdded.listen((event) {
       print(event.snapshot.value.toString());
       setState(() {
@@ -36,11 +39,34 @@ class _PersonalBoard extends State<PersonalBoard> {
     });
   }
 
+  // Future<List> ImageDownload() async{
+  //   FirebaseStorage _firebaseStorage = FirebaseStorage.instance;
+  //   StorageReference storageReference = _firebaseStorage.ref().child("map/1625230581747.png");
+  //   String downloadImg = await storageReference.getDownloadURL();
+  //   StorageReference storageReference1 = _firebaseStorage.ref().child("map/1625239722429.png");
+  //   String downloadImg1 = await storageReference1.getDownloadURL();
+  //   print(downloadImg1);
+  //   List a=[downloadImg,downloadImg1];
+  //   return a;
+  // }
+  GridView _imageGrid() {
+    return GridView.count(
+      shrinkWrap: true,
+      crossAxisCount: 3,
+      childAspectRatio: 1,
+      children: [
+        Image.network(_imgUrl[0].mapUrl, fit: BoxFit.fill),
+        Image.network(_imgUrl[1].mapUrl, fit: BoxFit.fill)
+      ],
+    );
+  }
+
   Widget build(BuildContext context) {
     //_imgUrl=List.from(_imgUrl.reversed);
     var screenWidth = MediaQuery.of(context).size.width;
     var screenHeight = MediaQuery.of(context).size.height;
     _imgUrl = List.from(_imgUrl.reversed);
+    print('ddddddd' + _imgUrl[0].mapUrl);
     return Scaffold(
       body: Container(
         child: Column(children: [
@@ -70,70 +96,68 @@ class _PersonalBoard extends State<PersonalBoard> {
                       ),
                     ),
                     Container(
-                    child: Column(
-                    children: [
-                    Padding(
-                    padding: EdgeInsets.fromLTRB(screenWidth*0.1, screenHeight*0.02, 0, 0),
-                    child: Text("Running",
-                    style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white))),
-                    Padding(
-                    padding: EdgeInsets.fromLTRB(screenWidth*0.12, screenHeight*0.03, 0, 0),
-                    child: Text("10km",
-                    style: TextStyle(color: Colors.white))),
-                    ],
-                  )),
-
-                Container(
-                  child: Column(
-                    children: [
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(screenWidth*0.13, screenHeight*0.02, 0, 0),
-                      child: Text("Time",
-                        style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white))),
-                      Padding(
-                        padding: EdgeInsets.fromLTRB(screenWidth*0.12, screenHeight*0.03, 0, 0),
-                        child: Text("14h",
-                        style: TextStyle(color: Colors.white))),
+                        child: Column(
+                          children: [
+                            Padding(
+                                padding: EdgeInsets.fromLTRB(screenWidth*0.1, screenHeight*0.02, 0, 0),
+                                child: Text("Running",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white))),
+                            Padding(
+                                padding: EdgeInsets.fromLTRB(screenWidth*0.12, screenHeight*0.03, 0, 0),
+                                child: Text("10km",
+                                    style: TextStyle(color: Colors.white))),
+                          ],
+                        )),
+                    Container(
+                        child: Column(
+                          children: [
+                            Padding(
+                                padding: EdgeInsets.fromLTRB(screenWidth*0.13, screenHeight*0.02, 0, 0),
+                                child: Text("Time",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white))),
+                            Padding(
+                                padding: EdgeInsets.fromLTRB(screenWidth*0.12, screenHeight*0.03, 0, 0),
+                                child: Text("14h",
+                                    style: TextStyle(color: Colors.white))),
+                          ],
+                        )),
+                  ],
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                  child: SizedBox(
+                    height: screenHeight*0.05,
+                    width: screenWidth*0.4,
+                    child: RaisedButton(
+                      color: Colors.white,
+                      child: Row(
+                        children: <Widget>[
+                          Icon(
+                            Icons.settings,
+                            color: Colors.deepOrange,
+                          ),
+                          Text('  setting',
+                              style: TextStyle(color: Colors.deepOrange))
                         ],
-                      )),
-                    ],
-                  ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-                child: SizedBox(
-                  height: screenHeight*0.05,
-                  width: screenWidth*0.4,
-                  child: RaisedButton(
-                    color: Colors.white,
-                    child: Row(
-                      children: <Widget>[
-                      Icon(
-                      Icons.settings,
-                      color: const Color(0xFF88C26F),
-                    ),
-                      Text('  setting',
-                      style: TextStyle(color: const Color(0xFF88C26F)))
-                    ],
-                     mainAxisAlignment: MainAxisAlignment.center,
-                    ),
-                    onPressed: () {
-                      reference.child(id).onChildAdded.listen((event) {
-                      user = User.fromSnapshot(event.snapshot);
-                      Navigator.of(context)
-                          .pushNamed('/mypage', arguments: user);
+                        mainAxisAlignment: MainAxisAlignment.center,
+                      ),
+                      onPressed: () {
+                        reference.child(id).onChildAdded.listen((event) {
+                          user = User.fromSnapshot(event.snapshot);
+                          Navigator.of(context)
+                              .pushNamed('/mypage', arguments: user);
                         });
                       },
+                    ),
                   ),
                 ),
-              ),
               ],
             ),
           ),
-
           _imgUrl.length == 0
               ? CircularProgressIndicator()
               : Expanded(
@@ -154,8 +178,7 @@ class _PersonalBoard extends State<PersonalBoard> {
                           //누르면 인스타 개인 게시물처럼 보기
                         },
                         child:
-                        //Text(_imgUrl[index].mapUrl),
-
+                        // Text('ddd'),
                         Image.network(_imgUrl[index].mapUrl,
                             fit: BoxFit.fill),
                       ),

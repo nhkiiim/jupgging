366c7c13ece912dbe9c959a320c13fc11d8fa3c8
hye_ppy <1569369@naver.com>
2021-07-10 16:59:14 +0900

    update
    update image model

--- lib/models/image.dart
+++ lib/models/image.dart
@@ -2,19 +2,31 @@ import 'package:firebase_database/firebase_database.dart';
 
 class ImageURL {
   String key;
-  String url;
+  String mapUrl;
+  String trashUrl;
+  String distance;
+  String time;
+  String comment;
   String createTime;
 
-  ImageURL(this.url, this.createTime);
+  ImageURL(this.mapUrl, this.trashUrl, this.distance, this.time, this.comment, this.createTime);
 
   ImageURL.fromSnapshot(DataSnapshot snapshot)
       : key = snapshot.key,
-        url = snapshot.value['url'],
+        mapUrl = snapshot.value['mapUrl'],
+        trashUrl = snapshot.value['trashUrl'],
+        distance = snapshot.value['distance'],
+        time = snapshot.value['time'],
+        comment = snapshot.value['comment'],
         createTime = snapshot.value['createTime'];
 
   toJson() {
     return {
-      'url': url,
+      'mapUrl': mapUrl,
+      'trashUrl': trashUrl,
+      'distance': distance,
+      'time': time,
+      'comment' : comment,
       'createTime': createTime,
     };
   }

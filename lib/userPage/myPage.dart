935f8673e13ccb2434ac009f76e7fb6890f32328
hye_ppy <1569369@naver.com>
2021-07-14 21:34:53 +0900

    Summary
    fix color

--- lib/userPage/myPage.dart
+++ lib/userPage/myPage.dart
@@ -35,190 +35,176 @@ class _MyPage extends State<MyPage> {
     _emailTextController = TextEditingController();
   }
 
-
-
-
   @override
   Widget build(BuildContext context) {
-    user = ModalRoute
-        .of(context)
-        .settings
-        .arguments;
+    user = ModalRoute.of(context).settings.arguments;
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('프로필 편집'),
+        title: Text(
+          '프로필 편집',
+          style: TextStyle(color: Colors.white),
+        ),
         actions: <Widget>[
-          FlatButton(onPressed: () {
-            if (_pwTextController.value.text.length == 0){
-              makeDialog('비밀번호를 입력해주세요');
-            } else{
-              var bytes = utf8.encode(_pwTextController.value.text);
-              var digest = sha1.convert(bytes);
-              if(user.pw == digest.toString()) {
-                if(user.email != _emailTextController.value.text){
-                  User upUser = User(user.name, user.id, user.pw,
-                      _emailTextController.value.text, user.createTime);
-                  reference
-                      .child(id)
-                      .child(user.key)
-                      .set(upUser.toJson())
-                      .then((_){
-                    Navigator.of(context).pop();
-                  });
-                }
-                else {
-                  makeDialog('기존 이메일과 똑같습니다');
+          FlatButton(
+            onPressed: () {
+              if (_pwTextController.value.text.length == 0) {
+                makeDialog('비밀번호를 입력해주세요');
+              } else {
+                var bytes = utf8.encode(_pwTextController.value.text);
+                var digest = sha1.convert(bytes);
+                if (user.pw == digest.toString()) {
+                  if (user.email != _emailTextController.value.text) {
+                    User upUser = User(user.name, user.id, user.pw,
+                        _emailTextController.value.text, user.createTime);
+                    reference
+                        .child(id)
+                        .child(user.key)
+                        .set(upUser.toJson())
+                        .then((_) {
+                      Navigator.of(context).pop();
+                    });
+                  } else {
+                    makeDialog('기존 이메일과 똑같습니다');
+                  }
+                } else {
+                  makeDialog('비밀번호가 일치하지 않습니다');
                 }
               }
-              else {
-                makeDialog('비밀번호가 일치하지 않습니다');
-              }
-            }
-          }, child: Text(
-            '완료',
-            style: TextStyle(color: Colors.white),
-          ),)
+            },
+            child: Text(
+              '완료',
+              style: TextStyle(color: Colors.white),
+            ),
+          )
         ],
       ),
       body: SingleChildScrollView(
         child: Container(
-            child: Center(
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    height: 200,
-                    child: Column(
-                      children: [
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(55.0),
-                          child: Image.asset(
-                            'image/tree.jpg',
-                            width: 90,
-                            height: 90,
-                            fit: BoxFit.fill,
-                          ),
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  height: 200,
+                  child: Column(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(55.0),
+                        child: Image.asset(
+                          'image/tree.jpg',
+                          width: 90,
+                          height: 90,
+                          fit: BoxFit.fill,
                         ),
-                        FlatButton(
-                            onPressed: () {},
-                            child: Text(
-                              '프로필 사진 바꾸기',
-                              style: TextStyle(color: Colors.blue),
-                            ))
-                      ],
-                      mainAxisAlignment: MainAxisAlignment.center,
-                    ),
+                      ),
+                      FlatButton(
+                          onPressed: () {},
+                          child: Text(
+                            '프로필 사진 바꾸기',
+                            style: TextStyle(color: Colors.blue),
+                          ))
+                    ],
+                    mainAxisAlignment: MainAxisAlignment.center,
                   ),
-                  Container(
-                    padding: EdgeInsets.all(20),
-                    child: Column(
-                      children: <Widget>[
-                        Row(
-                          children: <Widget>[
-                            SizedBox(
-                              width: 70,
-                              child: Text('이름'),
-                            ),
-                            Container(
-                              width: MediaQuery
-                                  .of(context)
-                                  .size
-                                  .width - 110,
-                              child: Text(user.name),
-                            ),
-                          ],
-                          //mainAxisAlignment: MainAxisAlignment.center,
-                        ),
-                        SizedBox(
-                          height: 12,
-                        ),
-                        Row(
-                          children: <Widget>[
-                            SizedBox(
-                              width: 70,
-                              child: Text('아이디'),
-                            ),
-                            Container(
-                              width: MediaQuery
-                                  .of(context)
-                                  .size
-                                  .width - 110,
-                              child: Text(user.id),
-                            ),
-                          ],
-                          //mainAxisAlignment: MainAxisAlignment.center,
-                        ),
-                        Row(
-                          children: <Widget>[
-                            SizedBox(
-                              width: 70,
-                              child: Text('이메일'),
-                            ),
-                            Container(
-                              width: MediaQuery
-                                  .of(context)
-                                  .size
-                                  .width - 110,
-                              child: TextField(
-                                controller: _emailTextController,
-                                maxLines: 1,
-                                decoration: InputDecoration(
-                                  hintText: user.email,
-                                ),
-                                style: TextStyle(fontSize: 14),
+                ),
+                Container(
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          SizedBox(
+                            width: 70,
+                            child: Text('이름'),
+                          ),
+                          Container(
+                            width: MediaQuery.of(context).size.width - 110,
+                            child: Text(user.name),
+                          ),
+                        ],
+                        //mainAxisAlignment: MainAxisAlignment.center,
+                      ),
+                      SizedBox(
+                        height: 12,
+                      ),
+                      Row(
+                        children: <Widget>[
+                          SizedBox(
+                            width: 70,
+                            child: Text('아이디'),
+                          ),
+                          Container(
+                            width: MediaQuery.of(context).size.width - 110,
+                            child: Text(user.id),
+                          ),
+                        ],
+                        //mainAxisAlignment: MainAxisAlignment.center,
+                      ),
+                      Row(
+                        children: <Widget>[
+                          SizedBox(
+                            width: 70,
+                            child: Text('이메일'),
+                          ),
+                          Container(
+                            width: MediaQuery.of(context).size.width - 110,
+                            child: TextField(
+                              controller: _emailTextController,
+                              maxLines: 1,
+                              decoration: InputDecoration(
+                                hintText: user.email,
                               ),
+                              style: TextStyle(fontSize: 14),
                             ),
-                          ],
-                          //mainAxisAlignment: MainAxisAlignment.center,
-                        ),
-                        Row(
-                          children: <Widget>[
-                            SizedBox(
-                              width: 70,
-                              child: Text('비밀번호'),
-                            ),
-                            Container(
-                              width: MediaQuery
-                                  .of(context)
-                                  .size
-                                  .width - 110,
-                              child: TextField(
-                                controller: _pwTextController,
-                                maxLines: 1,
-                                decoration: InputDecoration(
-                                  hintText: '비밀번호를 입력해주세요',
-                                ),
-                                style: TextStyle(fontSize: 14),
+                          ),
+                        ],
+                        //mainAxisAlignment: MainAxisAlignment.center,
+                      ),
+                      Row(
+                        children: <Widget>[
+                          SizedBox(
+                            width: 70,
+                            child: Text('비밀번호'),
+                          ),
+                          Container(
+                            width: MediaQuery.of(context).size.width - 110,
+                            child: TextField(
+                              controller: _pwTextController,
+                              maxLines: 1,
+                              decoration: InputDecoration(
+                                hintText: '비밀번호를 입력해주세요',
                               ),
+                              style: TextStyle(fontSize: 14),
                             ),
-                          ],
-                          //mainAxisAlignment: MainAxisAlignment.center,
-                        ),
-                        Row(
-                          children: <Widget>[
-                            SizedBox(
-                              width: 70,
-                            ),
-                            FlatButton(
-                                onPressed: () {
-                                  Navigator.of(context).pushReplacementNamed(
-                                      '/pwChange',
-                                      arguments: id);
-                                },
-                                child: Text(
-                                  '비밀번호 변경',
-                                  style: TextStyle(),
-                                ))
-                          ],
-                          //mainAxisAlignment: MainAxisAlignment.center,
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        Container(
-                            child: FlatButton(
-                                onPressed: () {
-                                  showDialog(
+                          ),
+                        ],
+                        //mainAxisAlignment: MainAxisAlignment.center,
+                      ),
+                      Row(
+                        children: <Widget>[
+                          SizedBox(
+                            width: 70,
+                          ),
+                          FlatButton(
+                              onPressed: () {
+                                Navigator.of(context).pushReplacementNamed(
+                                    '/pwChange',
+                                    arguments: id);
+                              },
+                              child: Text(
+                                '비밀번호 변경',
+                                style: TextStyle(),
+                              ))
+                        ],
+                        //mainAxisAlignment: MainAxisAlignment.center,
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                          child: FlatButton(
+                              onPressed: () {
+                                showDialog(
                                     context: context,
                                     builder: (context) {
                                       return AlertDialog(
@@ -226,48 +212,49 @@ class _MyPage extends State<MyPage> {
                                         content: Text('회원탈퇴 하시겠습니까?'),
                                         actions: <Widget>[
                                           FlatButton(
-                                              onPressed: (){
+                                              onPressed: () {
                                                 reference
                                                     .child(user.id)
                                                     .remove()
                                                     .then((_) {
-                                                      Navigator.of(context).pop();
-                                                      Navigator.of(context).pop();
-                                                      Navigator.of(context).pushReplacementNamed(
-                                                        '/login',);
+                                                  Navigator.of(context).pop();
+                                                  Navigator.of(context).pop();
+                                                  Navigator.of(context)
+                                                      .pushReplacementNamed(
+                                                    '/login',
+                                                  );
                                                 });
                                               },
                                               child: Text('예')),
                                           FlatButton(
-                                              onPressed: (){
+                                              onPressed: () {
                                                 Navigator.of(context).pop();
                                               },
                                               child: Text('아니오'))
                                         ],
                                       );
-                                    }
-                                  );
-                                },
-                                child: Text(
-                                  '회원 탈퇴하기',
-                                  style: TextStyle(color: Colors.blue),
-                                ))),
-                        Container(
-                            child: FlatButton(
-                                onPressed: () {},
-                                child: Text(
-                                  '로그아웃',
-                                  style: TextStyle(color: Colors.blue),
-                                ))),
-                      ],
-                    ),
+                                    });
+                              },
+                              child: Text(
+                                '회원 탈퇴하기',
+                                style: TextStyle(color: Colors.blue),
+                              ))),
+                      Container(
+                          child: FlatButton(
+                              onPressed: () {},
+                              child: Text(
+                                '로그아웃',
+                                style: TextStyle(color: Colors.blue),
+                              ))),
+                    ],
                   ),
-                ],
-                //mainAxisAlignment: MainAxisAlignment.center,
-              ),
+                ),
+              ],
+              //mainAxisAlignment: MainAxisAlignment.center,
             ),
           ),
         ),
+      ),
     );
   }
 
@@ -280,5 +267,4 @@ class _MyPage extends State<MyPage> {
           );
         });
   }
-
 }

d6c6682e62e04cfd2cc2892d15609809de558250
hye_ppy <1569369@naver.com>
2021-07-29 22:32:46 +0900

    Summary
    fix auto login

--- lib/boardPage/personalBoard.dart
+++ lib/boardPage/personalBoard.dart
@@ -19,59 +19,63 @@ class _PersonalBoard extends State<PersonalBoard> {
   FirebaseDatabase _database;
   DatabaseReference reference;
   DatabaseReference referenceImg;
-  URL url=URL();
+  URL url = URL();
   String _databaseURL;
-  int totalHour,totalMinute,totalSec;
+  int totalHour, totalMinute, totalSec;
   double totalDistance;
   static final storage = new FlutterSecureStorage();
+
   @override
   void initState() {
     super.initState();
-    _databaseURL=url.databaseURL;
+    _databaseURL = url.databaseURL;
     WidgetsBinding.instance.addPostFrameCallback((_) {
       _asyncMethod();
     });
     totalDistance = 0;
-    totalHour=0;
-    totalMinute=0;
-    totalSec=0;
+    totalHour = 0;
+    totalMinute = 0;
+    totalSec = 0;
     _database = FirebaseDatabase(databaseURL: _databaseURL);
     reference = _database.reference().child('user');
     referenceImg = _database.reference().child('image');
 
-    // reference.child(id).onChildAdded.listen((event) {
-    //   user=User.fromSnapshot(event.snapshot);
-    // });
-
     // ImageDownload().then((value)=>{
     //   setState(() {_imgUrl = value;})
     // });
-    referenceImg..orderByChild("id")
-        .equalTo(id).onChildAdded.listen((event) {
-      print(event.snapshot.value.toString());
+  }
+
+  _asyncMethod() async {
+    String _id = await storage.read(key: "login");
+    setState(() {
+      id = _id;
+    });
+    referenceImg.orderByChild("id").equalTo(id).onChildAdded.listen((event) {
+      //print(event.snapshot.value.toString());
       setState(() {
         ImageURL temp = ImageURL.fromSnapshot(event.snapshot);
         _imgUrl.add(temp);
-        print(double.parse(temp.distance));
+        //print(double.parse(temp.distance));
         totalDistance += double.parse(temp.distance);
-        totalHour += int.parse(temp.time.substring(0,2));
-        totalMinute += int.parse(temp.time.substring(3,5));
-        totalSec += int.parse(temp.time.substring(6,8));
+        totalHour += int.parse(temp.time.substring(0, 2));
+        totalMinute += int.parse(temp.time.substring(3, 5));
+        totalSec += int.parse(temp.time.substring(6, 8));
 
-        if(totalSec>=60){
-          totalMinute+=1;
-          totalSec%=60;
+        if (totalSec >= 60) {
+          totalMinute += 1;
+          totalSec %= 60;
         }
-        if(totalMinute>=60){
-          totalHour+=1;
-          totalMinute%=60;
+        if (totalMinute >= 60) {
+          totalHour += 1;
+          totalMinute %= 60;
         }
       });
     });
+    reference.child(id).onChildAdded.listen((event) {
+      user = User.fromSnapshot(event.snapshot);
+    });
   }
-  _asyncMethod() async {
-    id=await storage.read(key: "login");
-  }
+
   Widget build(BuildContext context) {
     _imgUrl = List.from(_imgUrl.reversed);
     var screenWidth = MediaQuery.of(context).size.width;
@@ -82,67 +86,67 @@ class _PersonalBoard extends State<PersonalBoard> {
           Container(
             color: const Color(0xFF88C26F),
             width: screenWidth,
-            height: screenHeight*0.3,
+            height: screenHeight * 0.3,
             child: Column(
               children: [
                 Row(
                   children: [
                     Padding(
                       padding: EdgeInsets.fromLTRB(
-                          (screenWidth*0.3-screenHeight*0.12)*0.7,
-                          screenHeight*0.07,
-                          (screenWidth*0.3-screenHeight*0.12)*0.5,
-                          screenHeight*0.04
-                      ),
+                          (screenWidth * 0.3 - screenHeight * 0.12) * 0.7,
+                          screenHeight * 0.07,
+                          (screenWidth * 0.3 - screenHeight * 0.12) * 0.5,
+                          screenHeight * 0.04),
                       child: ClipRRect(
                         borderRadius: BorderRadius.circular(55.0),
-                        child:
-                        //Image.network(user.profileImg, fit: BoxFit.fill)
-                        Image.asset(
-                          'image/tree.jpg',
-                          width: screenHeight*0.12,
-                          height: screenHeight*0.12,
-                          fit: BoxFit.fill,
-                        ),
+                        child: Image.network(user.profileImg,
+                            width: screenHeight * 0.12,
+                            height: screenHeight * 0.12,
+                            fit: BoxFit.fill),
                       ),
                     ),
                     Container(
                         child: Column(
-                          children: [
-                            Padding(
-                                padding: EdgeInsets.fromLTRB(screenWidth*0.1, screenHeight*0.02, 0, 0),
-                                child: Text("Running",
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.white))),
-                            Padding(
-                                padding: EdgeInsets.fromLTRB(screenWidth*0.12, screenHeight*0.03, 0, 0),
-                                child: Text(totalDistance.toString()+"km",
-                                    style: TextStyle(color: Colors.white))),
-                          ],
-                        )),
+                      children: [
+                        Padding(
+                            padding: EdgeInsets.fromLTRB(
+                                screenWidth * 0.1, screenHeight * 0.02, 0, 0),
+                            child: Text("Running",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white))),
+                        Padding(
+                            padding: EdgeInsets.fromLTRB(
+                                screenWidth * 0.12, screenHeight * 0.03, 0, 0),
+                            child: Text(totalDistance.toString() + "km",
+                                style: TextStyle(color: Colors.white))),
+                      ],
+                    )),
                     Container(
                         child: Column(
-                          children: [
-                            Padding(
-                                padding: EdgeInsets.fromLTRB(screenWidth*0.13, screenHeight*0.02, 0, 0),
-                                child: Text("Time",
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.white))),
-                            Padding(
-                                padding: EdgeInsets.fromLTRB(screenWidth*0.12, screenHeight*0.03, 0, 0),
-                                child: Text("${totalHour.toString().padLeft(2, '0')}:${totalMinute.toString().padLeft(2, '0')}:${totalSec.toString().padLeft(2, '0')}",
-                                    style: TextStyle(color: Colors.white))),
-                          ],
-                        )),
+                      children: [
+                        Padding(
+                            padding: EdgeInsets.fromLTRB(
+                                screenWidth * 0.13, screenHeight * 0.02, 0, 0),
+                            child: Text("Time",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white))),
+                        Padding(
+                            padding: EdgeInsets.fromLTRB(
+                                screenWidth * 0.12, screenHeight * 0.03, 0, 0),
+                            child: Text(
+                                "${totalHour.toString().padLeft(2, '0')}:${totalMinute.toString().padLeft(2, '0')}:${totalSec.toString().padLeft(2, '0')}",
+                                style: TextStyle(color: Colors.white))),
+                      ],
+                    )),
                   ],
                 ),
                 Padding(
                   padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
                   child: SizedBox(
-                    height: screenHeight*0.05,
-                    width: screenWidth*0.4,
+                    height: screenHeight * 0.05,
+                    width: screenWidth * 0.4,
                     child: RaisedButton(
                       color: Colors.white,
                       child: Row(
@@ -157,11 +161,10 @@ class _PersonalBoard extends State<PersonalBoard> {
                         mainAxisAlignment: MainAxisAlignment.center,
                       ),
                       onPressed: () {
-                        reference.child(id).onChildAdded.listen((event) {
-                          user = User.fromSnapshot(event.snapshot);
-                          Navigator.of(context)
-                              .pushNamed('/mypage', arguments: user);
-                        });
+                        //reference.child(id).onChildAdded.listen((event) {
+                        //user = User.fromSnapshot(event.snapshot);
+                        Navigator.of(context).pushNamed('/mypage');
+                        //});
                       },
                     ),
                   ),
@@ -169,41 +172,41 @@ class _PersonalBoard extends State<PersonalBoard> {
               ],
             ),
           ),
-          _imgUrl.length == 0 ? CircularProgressIndicator() : Expanded(
-            child: GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3),
-              scrollDirection: Axis.vertical,
-              shrinkWrap: true,
-              itemBuilder: (context, index) {
-                return Card(
-                  //child: GridTile(
-                  child: Container(
-                    //width: 200,
-                    //padding: EdgeInsets.only(top: 20, bottom: 20),
-                    child: SizedBox(
-                      child: GestureDetector(
-                        onTap: () {
-                          //누르면 인스타 개인 게시물처럼 보기
-                          Navigator.of(context)
-                              .pushNamed('/detail',
-                              arguments: _imgUrl[index]
-                          );
-                        },
-                        child:
-                        //Text(_imgUrl[index].mapUrl),
+          _imgUrl.length == 0
+              ? CircularProgressIndicator()
+              : Expanded(
+                  child: GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 3),
+                    scrollDirection: Axis.vertical,
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        //child: GridTile(
+                        child: Container(
+                          //width: 200,
+                          //padding: EdgeInsets.only(top: 20, bottom: 20),
+                          child: SizedBox(
+                            child: GestureDetector(
+                              onTap: () {
+                                //누르면 인스타 개인 게시물처럼 보기
+                                Navigator.of(context).pushNamed('/detail',
+                                    arguments: _imgUrl[index]);
+                              },
+                              child:
+                                  //Text(_imgUrl[index].mapUrl),
 
-                        Image.network(_imgUrl[index].mapUrl,
-                            fit: BoxFit.fill),
-                      ),
-                    ),
+                                  Image.network(_imgUrl[index].mapUrl,
+                                      fit: BoxFit.fill),
+                            ),
+                          ),
+                        ),
+                        //),
+                      );
+                    },
+                    itemCount: _imgUrl.length,
                   ),
-                  //),
-                );
-              },
-              itemCount: _imgUrl.length,
-            ),
-          ),
+                ),
         ]),
       ),
     );
